ce18486ff8b1929cd31475e0eec71064bd7e8be5
PNDA11 <pwintnadiaung386@gmail.com>
2025-10-24 15:33:19 +0900

    横6,縦4で24個

--- form_test/form_test/Form1.cs
+++ form_test/form_test/Form1.cs
@@ -15,6 +15,23 @@ namespace form_test
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j =0; j < 4; j++)
+                {
+                    テキストボタン testbutton = new テキストボタン();
+                    testbutton.Location = new Point(i*100,j*50);
+                    testbutton.Size = new Size(50, 50);
+                    testbutton.Text = "あいうえお";
+                    Controls.Add(testbutton);
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("C#の世界へようこそ!");
         }
     }
 }
