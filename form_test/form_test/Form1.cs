ff03e985ceb76123fe0c4c822a7326271552dc38
PNDA11 <pwintnadiaung386@gmail.com>
2025-10-31 15:56:24 +0900

    １０月３１日終了

--- form_test/form_test/Form1.cs
+++ form_test/form_test/Form1.cs
@@ -12,27 +12,42 @@ namespace form_test
 {
     public partial class Form1 : Form
     {
+        //constをつけると初期化時にのみ値の変更が可能になる
+        //ボタンの横幅
+        const int BUTTON_SIZE_X = 100;
+        //ボタンの縦
+        const int BUTTON_SIZE_Y = 100;
+        //ボタンの横に何個並ぶか
+        const int BOARD_SIZE_X = 3;
+        //ボタンの縦に何個並ぶか
+        const int BOARD_SIZE_Y = 3;
+
+        private TestButton[,] _buttonArray;
+
         public Form1()
         {
             InitializeComponent();
-            for (int i = 0; i < 3; i++)
+            //buttonArayの初期化
+            _buttonArray=new TestButton[BOARD_SIZE_Y, BOARD_SIZE_X];
+            for (int i = 0; i < BOARD_SIZE_X; i++)
             {
-                for (int j =0; j < 3; j++)
+                for (int j =0; j < BOARD_SIZE_Y; j++)
                 {//
-                    TestButton testButton = new TestButton(new Point(i * 100, j * 50),new Size(50, 50),"あいうえお");
-                    //ボタンの位置を設定
-                   // testbutton.Location = new Point(i*100,j*50);
-                    //ボタンの大きさを設定
-                    //testbutton.Size = ;
-                    //testbutton.Text = ;
+                    TestButton testButton = 
+                        new TestButton(new Point(BUTTON_SIZE_X*i, BUTTON_SIZE_Y*j),
+                                       new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y),"あいうえお");
+                    //配列にボタンの参照を追加
+                    _buttonArray[j,i]=testButton;
                     //buttonを押すとmessageBoxの中のやつが出る
                     //Click Event に　hogehogeClick関数を登録
                     //click ＝押すときに字がでる
                     //Ｍｏｕｓｅｏｖｅｒ＝mouseをのせるだけで映す
-                    
+                    //controlにbutton
                     Controls.Add(testButton);
                 }
             }
+            _buttonArray[1,0 ].SetEnable(true);
+           
 
         }
       
--- form_test/form_test/TestButton.cs
+++ form_test/form_test/TestButton.cs
@@ -10,18 +10,52 @@ namespace form_test
 {
     internal class TestButton:Button
     {
+        //onの時の色
+        private Color _onColor=Color.Beige;
+        //offの時の色
+        private Color _offColor = Color.Brown;
+        //現在　on か　off か
+        //押すと色変わる
+        private bool _enable;
+
+        //onとoffの設定
+        public void SetEnable(bool on)
+        {
+            _enable = on;  
+            if(on)
+            {
+                BackColor=_onColor;
+            }
+            else
+            {
+                BackColor=_offColor;
+            }
+            
+        }
+
+
         public TestButton(Point position,Size size,string text)
         {
+            //ボタンの位置の設定
             Location = position;
+            //ボタンの大きさの設定
             Size = size;
+            //ボタン内のテキストの設定
             Text = text;
+
+            SetEnable(false);
             Click += ClickEvent;
 
         }
+        //自分で作成することも可能
           private void ClickEvent(object sender,EventArgs e)
         {
-            MessageBox.Show("クリックされてしまいました");
+            //( )のなかにtrueを入れるとtrue の色だけに変わる
+            //( )のなかにfalseを入れるとfalse の色だけに変わる
+
+            SetEnable(!_enable);
         }
+
 
     }
 }
